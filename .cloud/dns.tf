4de656aefb284f0570d8de653da6138d7f570cb0
James Cross <james@breakingwave.com>
2022-05-18 14:37:32 +0100

    Adding lychee

--- .cloud/dns.tf
+++ .cloud/dns.tf
@@ -20,6 +20,14 @@ resource "azurerm_dns_cname_record" "photoprism" {
   record              = azurerm_dns_a_record.home.fqdn
 }
 
+resource "azurerm_dns_cname_record" "lychee" {
+  name                = "photos2"
+  zone_name           = azurerm_dns_zone.root.name
+  resource_group_name = azurerm_resource_group.personalcloud.name
+  ttl                 = 300
+  record              = azurerm_dns_a_record.home.fqdn
+}
+
 resource "azurerm_dns_cname_record" "syncthing" {
   name                = "sync"
   zone_name           = azurerm_dns_zone.root.name
