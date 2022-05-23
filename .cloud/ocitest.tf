42cc62ac69fdb6d443e90afa95fcf7e27c598f64
James Cross <james@breakingwave.com>
2022-05-23 15:49:48 +0100

    Duplicate

--- .cloud/ocitest.tf
+++
@@ -1,9 +0,0 @@
-# Get a list of Availability Domains
-data "oci_identity_availability_domains" "ads" {
-  compartment_id = "${var.oci_tenancy_ocid}"
-}
-
-# Output the result
-output "show-ads" {
-  value = "${data.oci_identity_availability_domains.ads.availability_domains}"
-}
