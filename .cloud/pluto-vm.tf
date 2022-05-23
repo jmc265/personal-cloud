ca5355518ac8f9305fbf69888c169a9926633471
James Cross <james@breakingwave.com>
2022-05-23 11:13:51 +0100

    Enable Https

--- .cloud/pluto-vm.tf
+++ .cloud/pluto-vm.tf
@@ -97,7 +97,7 @@ resource "google_compute_firewall" "http-server" {
 
   allow {
     protocol = "tcp"
-    ports    = ["80"]
+    ports    = ["80", "443"]
   }
 
   // Allow traffic from everywhere to instances with an http-server tag
