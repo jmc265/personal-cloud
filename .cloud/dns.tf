c13931da9d36f3a1855e87f87c446946fbb9980c
James Cross <james@breakingwave.com>
2022-05-24 09:38:57 +0100

    Attempting wildcard DNS for jupiter

--- .cloud/dns.tf
+++ .cloud/dns.tf
@@ -4,76 +4,20 @@ resource "azurerm_dns_zone" "root" {
 }
 
 # This will act as my DDNS record
-resource "azurerm_dns_a_record" "home" {
-  name                = "home"
+resource "azurerm_dns_a_record" "jupiter" {
+  name                = "jupiter"
   zone_name           = azurerm_dns_zone.root.name
   resource_group_name = azurerm_resource_group.personalcloud.name
   ttl                 = 300
   records             = ["1.2.3.4"] # Updated by script
 }
 
-resource "azurerm_dns_cname_record" "photoprism" {
-  name                = "photos"
+resource "azurerm_dns_cname_record" "jupiter_wildcard" {
+  name                = "*"
   zone_name           = azurerm_dns_zone.root.name
   resource_group_name = azurerm_resource_group.personalcloud.name
   ttl                 = 300
-  record              = azurerm_dns_a_record.home.fqdn
-}
-
-resource "azurerm_dns_cname_record" "syncthing" {
-  name                = "sync"
-  zone_name           = azurerm_dns_zone.root.name
-  resource_group_name = azurerm_resource_group.personalcloud.name
-  ttl                 = 300
-  record              = azurerm_dns_a_record.home.fqdn
-}
-
-resource "azurerm_dns_cname_record" "homeassistant" {
-  name                = "homeassistant"
-  zone_name           = azurerm_dns_zone.root.name
-  resource_group_name = azurerm_resource_group.personalcloud.name
-  ttl                 = 300
-  record              = azurerm_dns_a_record.home.fqdn
-}
-
-resource "azurerm_dns_cname_record" "recipes" {
-  name                = "recipes"
-  zone_name           = azurerm_dns_zone.root.name
-  resource_group_name = azurerm_resource_group.personalcloud.name
-  ttl                 = 300
-  record              = azurerm_dns_a_record.home.fqdn
-}
-
-resource "azurerm_dns_cname_record" "media" {
-  name                = "media"
-  zone_name           = azurerm_dns_zone.root.name
-  resource_group_name = azurerm_resource_group.personalcloud.name
-  ttl                 = 300
-  record              = azurerm_dns_a_record.home.fqdn
-}
-
-resource "azurerm_dns_cname_record" "cloud" {
-  name                = "cloud"
-  zone_name           = azurerm_dns_zone.root.name
-  resource_group_name = azurerm_resource_group.personalcloud.name
-  ttl                 = 300
-  record              = azurerm_dns_a_record.home.fqdn
-}
-
-resource "azurerm_dns_cname_record" "start" {
-  name                = "start"
-  zone_name           = azurerm_dns_zone.root.name
-  resource_group_name = azurerm_resource_group.personalcloud.name
-  ttl                 = 300
-  record              = azurerm_dns_a_record.home.fqdn
-}
-
-resource "azurerm_dns_cname_record" "healthchecks" {
-  name                = "healthchecks"
-  zone_name           = azurerm_dns_zone.root.name
-  resource_group_name = azurerm_resource_group.personalcloud.name
-  ttl                 = 300
-  record              = azurerm_dns_a_record.home.fqdn
+  record              = azurerm_dns_a_record.jupiter.fqdn
 }
 
 resource "azurerm_dns_a_record" "pluto" {
