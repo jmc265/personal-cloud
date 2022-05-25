ca07506f65d8f4154b5986acdd302afebc1517e9
James Cross <james@breakingwave.com>
2022-05-25 10:05:06 +0100

    Getting values out into an object

--- jupiter/photoprism/mirrorExif/src/index.ts
+++ jupiter/photoprism/mirrorExif/src/index.ts
@@ -3,7 +3,7 @@ import { exec } from "child_process";
 const SQL_PASSWORD = process.env.SQL_PASSWORD;
 
 async function executeSql(sql: string): Promise<any> {
-    const dockerCommand = `docker exec mariadb mariadb --user root -p${SQL_PASSWORD} -D photoprism -N -t -E -e "${sql}"`;
+    const dockerCommand = `docker exec mariadb mariadb --user root -p${SQL_PASSWORD} -D photoprism -N -t -e "${sql}"`;
     return new Promise((resolve, reject) => {
         exec(dockerCommand, (error, stdout, stderr) => {
             if (error) {
@@ -17,9 +17,18 @@ async function executeSql(sql: string): Promise<any> {
     });
 }
 
-async function getImageDescriptionUpdates(): Promise<{file_name: string, photo_description: string}> {
+async function getImageDescriptionUpdates(): Promise<{file_name: string, photo_description: string}[]> {
     const sql = `SELECT files.file_name, photos.photo_description FROM photos JOIN files ON files.photo_id = photos.id WHERE photos.description_src='manual';`
-    return executeSql(sql);
+    const sqlResponse: string = await executeSql(sql);
+    return sqlResponse.split("\n")
+        .filter(line => !line.startsWith("+--"))
+        .map(line => {
+            const values = line.split("|");
+            return {
+                file_name: values[0],
+                photo_description: values[1]
+            }   
+        });
 }
 
 async function main() {
