c2bee95ec2a01706a7bc2763d9be669365736f62
James Cross <me@james.cx>
2023-02-26 22:10:56 +0000

    Pluto NewRelic

--- .cloud/newrelic.tf
+++ .cloud/newrelic.tf
@@ -18,3 +18,31 @@ resource "newrelic_cloud_azure_integrations" "foo" {
     key_vault {}
     storage {}
 }
+
+resource "newrelic_cloud_gcp_link_account" "nr_account_link_gcp" {
+  name = "${var.short_prefix}linkgcp"
+  project_id=var.gcp_project_id
+}
+
+resource "newrelic_cloud_gcp_integrations" "foo1" {
+  linked_account_id = newrelic_cloud_gcp_link_account.nr_account_link_gcp.id
+  app_engine {
+    metrics_polling_interval = 400
+  }
+  data_store {
+    metrics_polling_interval = 400
+  }
+  run {
+    metrics_polling_interval = 400
+  }
+  storage {
+    metrics_polling_interval = 400
+    fetch_tags=true
+  }
+  virtual_machines {
+    metrics_polling_interval = 400
+  }
+  vpc_access {
+    metrics_polling_interval = 400
+  }
+}
