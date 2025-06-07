2a013f0db0d7b25f09ce6c720c419e6222c1c7f6
james <jamescross265@gmail.com>
2025-06-07 20:57:22 +0100

    Add NSG

--- .cloud/pluto-vm-azure.tf
+++ .cloud/pluto-vm-azure.tf
@@ -109,6 +109,24 @@ resource "azurerm_virtual_machine_data_disk_attachment" "disk" {
   caching            = "ReadWrite"
 }
 
+resource "azurerm_network_security_group" "nsg" {
+  name                = "VM-NSG"
+  location            = azurerm_resource_group.personalcloud.location
+  resource_group_name = azurerm_resource_group.personalcloud.name
+
+  security_rule {
+    name                       = "ssh"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "22"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
 resource "azurerm_linux_virtual_machine" "pluto" {
   name                = "pluto"
   resource_group_name = azurerm_resource_group.personalcloud.name
